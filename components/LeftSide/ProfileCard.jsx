aead99a24d2a8feed5d7ed3096170e8525b1f0a7
Jinhyo <sosilion@gmail.com>
2020-09-17 21:07:47 +0900

    10. <ProfileCard /> front 수정

--- components/LeftSide/ProfileCard.jsx
+++ components/LeftSide/ProfileCard.jsx
@@ -1,37 +1,40 @@
 import React from "react";
-import { Card, Image, Button, Icon } from "semantic-ui-react";
+import { Card, Image, Button, Icon, Divider } from "semantic-ui-react";
 
 function ProfileCard(props) {
   return (
-    <Card>
-      <Card.Content>
-        <Image floated="left" size="small" src="../../../images/123.jpg" />
-        <Card.Header>JJ</Card.Header>
-        <Card.Meta>@Jin </Card.Meta>
-        <Card.Meta>
-          <Icon name="map marker alternate" />
-          서울
-        </Card.Meta>
-        <Card.Description>자기소개</Card.Description>
-      </Card.Content>
+    <>
+      <Card>
+        <Card.Content>
+          <Image floated="left" size="small" src="../../../images/123.jpg" />
+          <Card.Header>JJ</Card.Header>
+          <Card.Meta>@Jin </Card.Meta>
+          <Card.Meta>
+            <Icon name="map marker alternate" />
+            서울
+          </Card.Meta>
+          <Card.Description>자기소개</Card.Description>
+        </Card.Content>
 
-      <Card.Content extra>
-        <div className="ui three buttons">
-          <Button basic color="green">
-            <p>트윗</p>
-            <p>20</p>
-          </Button>
-          <Button basic color="green">
-            <p>팔로워</p>
-            <p>3</p>
-          </Button>
-          <Button basic color="green">
-            <p>팔로윙</p>
-            <p>5</p>
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
+        <Card.Content extra>
+          <div className="ui three buttons">
+            <Button basic color="green">
+              <p>트윗</p>
+              <p>20</p>
+            </Button>
+            <Button basic color="green">
+              <p>팔로워</p>
+              <p>3</p>
+            </Button>
+            <Button basic color="green">
+              <p>팔로윙</p>
+              <p>5</p>
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+      <Divider hidden section />
+    </>
   );
 }
 
