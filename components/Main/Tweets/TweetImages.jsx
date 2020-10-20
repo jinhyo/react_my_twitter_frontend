37e7d2f06bc50c42ab2780ff27df8ebcb61fbcfb
Jinhyo <sosilion@gmail.com>
2020-10-20 17:13:51 +0900

    116. 이미지 리사이징 수정: gif 파일은 원본 파일 사용, 방금 추가된 트윗의 경우 원본 이미지 파일 사용

--- components/Main/Tweets/TweetImages.jsx
+++ components/Main/Tweets/TweetImages.jsx
@@ -1,5 +1,6 @@
 import React, { useCallback, useState } from "react";
 import { Image } from "semantic-ui-react";
+import axios from "axios";
 
 import ImageZoomModal from "./ImageZoomModal";
 
@@ -15,13 +16,29 @@ function TweetImages({ images }) {
     setModal(true);
   }, []);
 
+  const setSrc = useCallback(async src => {
+    const ext = src.split(".")[src.split(".").length - 1];
+
+    if (ext === "gif") {
+      return src;
+    } else {
+      // 방금 추가된 트윗의 경우 리사이징 되기 전 이미지를 사용
+      const result = await axios.get(src);
+      if (result) {
+        return src.replace("/images/", "/thumb/");
+      } else {
+        return src;
+      }
+    }
+  }, []);
+
   return (
     <>
       <Image.Group>
         {images.map((image, index) => (
           <Image
             key={index}
-            src={image.src.replace("/images/", "/thumb/")}
+            src={setSrc(image.src)}
             width={200}
             height={200}
             bordered
