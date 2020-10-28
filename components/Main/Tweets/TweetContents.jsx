5ead4749ec4aba60b50055986376129e36f154a3
Jinhyo <sosilion@gmail.com>
2020-10-28 21:19:53 +0900

    수정

--- components/Main/Tweets/TweetContents.jsx
+++ components/Main/Tweets/TweetContents.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import userFunctions from "../../../lib/userFunctions";
 
 // in <TweetCard />
 function TweetContents({ contents }) {
+  const router = useRouter();
+
+  /* 검색결과 클릭 or 검색창에서 엔터 */
+  const linkToUserInfo = useCallback(async text => {
+      try {
+       const userId = await userFunctions.findUserId(text.slice(1))
+       router.push(`/users/${userId}`);
+      } catch (error) {
+        console.error(error);
+      }
+   
+  }, []);
+  
   return (
     <>
       {contents.split(/(#[^\s#]+|@[^\s@]+)/g).map((content, index) => {
@@ -18,13 +33,9 @@ function TweetContents({ contents }) {
           );
         } else if (content.match(/(@[^\s@]+)/g)) {
           return (
-            <Link
-              key={index}
-              href="/users/[userId]"
-              as={`/users/${content.slice(1)}`}
-            >
+            <span onClick={()=>linkToUserInfo(content)}>
               <a>{content}</a>
-            </Link>
+            </span>
           );
         }
         return content;
